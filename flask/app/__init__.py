3cbcb8edae258d60885e7f2810a4b4874acadae0
AlexCristache <mc14468@my.bristol.ac.uk>
2020-01-19 02:11:24 +0200

    add SQLAlchemy and Payment model

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,8 +1,13 @@
 """
 Init the app
 """
+# pylint: disable=C0103
+import os
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
-app = Flask(__name__) # pylint: disable=C0103
+app = Flask(__name__)
+SQLALCHEMY_DATABASE_URI = os.environ['DATBASE_URL']
+db = SQLAlchemy(app)
 
 from app import views # pylint: disable=c0413
---
+++ flask/app/models/payment.py
@@ -0,0 +1,30 @@
+"""
+The model for the SQLAlchemy payment object.
+"""
+# pylint: disable=R0903
+from app import db
+
+class Payment(db.Model):
+    """
+    Payment class that abstracts the database schema which records the
+    payments that were made.
+    """
+    __tablename__ = 'payments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    card_holder = db.Columnt(db.String(80), nullable=False)
+    card_number = db.Column(db.String(16), nullable=False)
+    expiration_date = db.Column(db.DateTime, nullable=False)
+    security_code = db.Column(db.String(3), nullable=True)
+    amount = db.Column(db.Integer, nullable=False)
+    attempted_date = db.Column(db.DateTime, nullable=False)
+
+
+    def __init__(self, **kwargs):
+        """
+        Init the values for the newly create payment object.
+        """
+        valid_keys = ("amount", "card_holder", "card_number",
+                      "expiration_date", "attempted_date", "security_code")
+        for key in valid_keys:
+            setattr(self, key, kwargs.get(key))
