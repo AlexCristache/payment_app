6b3e06196e4887b6cc4291300b4f0e0c397fc45d
AlexCristache <mc14468@my.bristol.ac.uk>
2020-01-19 23:19:03 +0200

    updated README and tests

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Flask app using Docker and NGINX
+
+For local development the sqlite database is used. In order to run the application run the flask app while inside the flask directory.
+An example usage of the api can be found in the tests.
+
+The docker container is setup to use PostgresSQL as database, but there are some issues with the connectivity I am yet to fix.
+
+Further improvements:
+- make PostgresSQL work inside the docker
+- write more unit tests
+- add logging to the application for better monitoring.
--- flask/tests/test_process_payment.py
+++ flask/tests/test_process_payment.py
@@ -10,32 +10,17 @@ from app import app
 
 from app.models.payment import Payment
 
-def test_endpoint():
-    """
-    Test that the endpoint is available.
-    """
-    with app.test_client() as c:
-        data = {"card_holder": "John Smith",
-            "card_number": "0000-0000-0000-0000",
-            "security_code": "333",
-            "amount": 10}
-        data_j = json.dumps(data)
-        response = c.post("http://localhost/payment/create",
-                          json=data_j)
-    #response = requests.post(url="http://localhost/create/payment", data=data_j,
-    #                         headers={"Content-Type": "application/json"})
-    assert response.status_code == 200
-
-
 def test_process_payment_create_db_entry():
     """
     Test that the API call creates the entry in the database.
     """
     with app.test_client() as c:
         data = {"card_holder": "John Smith",
-            "card_number": "0000-0000-0000-0000",
-            "security_code": "333",
-            "amount": 10}
+                "card_number": "0000-0000-0000-0000",
+                "security_code": "333",
+                "amount": 10,
+                "attempted_date": str(datetime.datetime.now()),
+                "expiration_date": str(datetime.datetime(2020, 12, 12))}
         data_j = json.dumps(data)
         response = c.post("http://localhost/payment/create", json=data_j)
 
